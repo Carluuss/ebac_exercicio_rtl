6964b96e30b9788b99d0aa00b03e59edb0213f2f
Carlos <128008806+Carluuss@users.noreply.github.com>
2024-08-05 15:10:10 -0300

    criando testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve adicionar 2 comentários", () => {
+    render(<PostComment />);
+
+    fireEvent.change(screen.getByTestId("textArea-comment"), {
+      target: {
+        value: "Comentário de teste",
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-comment"));
+
+    fireEvent.change(screen.getByTestId("textArea-comment"), {
+      target: {
+        value: "Segundo comentário de teste",
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-comment"));
+  });
 });
